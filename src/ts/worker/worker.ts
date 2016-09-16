67bf4ec0815ff1b5971d6a8060116a03385c1154
Alex Macleod <alex@macleod.io>
2016-09-16 22:55:53 +0100

    Use type guards

--- src/ts/worker/worker.ts
+++ src/ts/worker/worker.ts
@@ -1,20 +1,28 @@
 /// <reference path="../../../node_modules/typescript/lib/lib.webworker.d.ts" />
 
-import * as Messages from '../messages'
+import { InputMessage, TickMessage, WorkerMessage } from '../messages'
 import { State } from './state'
-import { receiveInput, receiveTick } from './receivers'
+import { getCommunicator } from './communicator'
 
 State.initialise()
 
 // TODO: track recieved and requested № entries in main thread
 
-onmessage = function ({data}: { data: Messages.Message }) {
-  switch (data.action) {
-    case 'input':
-      receiveInput(data as Messages.InputMessage)
-      break
-    case 'tick':
-      receiveTick(data as Messages.TickMessage)
-      break
+onmessage = function ({data: message}: { data: WorkerMessage }) {
+  if (isInput(message)) {
+    return getCommunicator(message.type)
+      .receive(message)
   }
+  if (isTick(message)) {
+    return getCommunicator()
+      .send()
+  }
+}
+
+function isInput(message: WorkerMessage): message is InputMessage {
+  return message.action === 'input'
+}
+
+function isTick(message: WorkerMessage): message is TickMessage {
+  return message.action === 'tick'
 }
