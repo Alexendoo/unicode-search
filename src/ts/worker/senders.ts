583ca25c43dd7e3427e028ef30658aed6b7d6c24
Alex Macleod <alex@macleod.io>
2016-09-17 15:29:51 +0100

    Correctly send DisplayMessage to browser for chars type

--- src/ts/worker/senders.ts
+++ src/ts/worker/senders.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../../node_modules/typescript/lib/lib.webworker.d.ts" />
 
 import { ClearMessage, DisplayMessage, InputType } from '../messages'
+import { State } from './state'
+import { stringToUtf8ByteArray } from '../util'
 
 export function sendClear(type: InputType) {
   const message: ClearMessage = {
@@ -12,14 +14,38 @@ export function sendClear(type: InputType) {
 }
 
 export function sendCharacter(input: string) {
+  const codePoint = input.codePointAt(0)
+
+  const block = getBlock(codePoint)
+  const bytes = getBytes(input)
+  const character = input
+  const name = (State.names) ? State.names[codePoint] || 'Unknown' : 'Loading…'
+
   const message: DisplayMessage = {
     action: 'display',
-    block: 'one',
-    bytes: 'AA',
-    character: input,
-    codePoint: 1,
-    name: 'character'
+    block,
+    bytes,
+    character,
+    codePoint,
+    name
   }
 
   self.postMessage(message)
+}
+
+function getBlock(codePoint: number): string {
+  if (!State.blocks) return 'Loading…'
+
+  for (const block of State.blocks) {
+    if (codePoint >= Number(block.start) && codePoint <= Number(block.end)) return block.name
+  }
+
+  return 'Unknown'
+}
+
+function getBytes(input: string): string {
+  return stringToUtf8ByteArray(input)
+    .map(byte => byte.toString(16))
+    .map(byte => byte.length < 2 ? '0' + byte : byte)
+    .join(' ')
 }
