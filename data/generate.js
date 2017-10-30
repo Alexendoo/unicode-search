76bbb7f1e287734c62ce36a7f490caf741adf8c4
Alex Macleod <alex@macleod.io>
2017-10-30 13:15:17 +0000

    formatting

--- data/generate.js
+++ data/generate.js
@@ -1,57 +1,55 @@
 #!/usr/bin/env node
-'use strict'
+"use strict"
 
-const fs = require('fs')
-const path = require('path')
-const sax = require('sax')
+const fs = require("fs")
+const path = require("path")
+const sax = require("sax")
 
 const saxStream = sax.createStream(true)
 
-{ // Characters
+{
+  // Characters
   let char = {
-    code: '',
-    name: '',
+    code: "",
+    name: "",
   }
 
-  saxStream.on('opentag', node => {
+  saxStream.on("opentag", node => {
     const attr = node.attributes
-    if (node.name !== 'char' || attr.na === undefined) return
-
-
+    if (node.name !== "char" || attr.na === undefined) return
 
     char.code = attr.cp
     char.name = attr.na.replace(/#/g, attr.cp)
   })
 
-  saxStream.on('opentag', node => {
-    if (node.name !== 'name-alias') return
+  saxStream.on("opentag", node => {
+    if (node.name !== "name-alias") return
     const attr = node.attributes
 
     switch (attr.type) {
-      case 'correction':
+      case "correction":
         char.name = attr.alias
         break
-      case 'control':
-      case 'figment':
-        if (char.name === '') char.name = attr.alias
+      case "control":
+      case "figment":
+        if (char.name === "") char.name = attr.alias
         break
     }
   })
 
-  saxStream.on('closetag', nodeName => {
-    if (nodeName !== 'char') return
+  saxStream.on("closetag", nodeName => {
+    if (nodeName !== "char") return
 
     console.log(char)
     char = {
-      code: '',
-      name: '',
+      code: "",
+      name: "",
     }
   })
 }
 
-saxStream.on('opentag', node => {
-  if (node.name !== 'block') return
+saxStream.on("opentag", node => {
+  if (node.name !== "block") return
 })
 
-fs.createReadStream(path.join(__dirname, 'ucd.test.flat.xml'))
-  .pipe(saxStream)
+fs.createReadStream(path.join(__dirname, "ucd.test.flat.xml")).pipe(saxStream)
