4b0e9a7c675ad6c450005ebaf403687beef7e776
Alex Macleod <alex@macleod.io>
2017-10-31 16:32:53 +0000

    correct name derivation rule prefix strings

--- data/generate.js
+++ data/generate.js
@@ -11,19 +11,19 @@ const saxStream = sax.createStream(true)
 // https://www.unicode.org/versions/Unicode10.0.0/ch04.pdf
 // Table 4-8. Name Derivation Rule Prefix Strings
 const nameDerivationRules = [
-  { start: 0xac00, end: 0xd7a3, prefix: "hangul syllable" },
-  { start: 0x3400, end: 0x4db5, prefix: "cjk unified ideograph-" },
-  { start: 0x4e00, end: 0x9fea, prefix: "cjk unified ideograph-" },
-  { start: 0x20000, end: 0x2a6d6, prefix: "cjk unified ideograph-" },
-  { start: 0x2a700, end: 0x2b734, prefix: "cjk unified ideograph-" },
-  { start: 0x2b740, end: 0x2b81d, prefix: "cjk unified ideograph-" },
-  { start: 0x2b820, end: 0x2cea1, prefix: "cjk unified ideograph-" },
-  { start: 0x2ceb0, end: 0x2ebe0, prefix: "cjk unified ideograph-" },
-  { start: 0x17000, end: 0x187ec, prefix: "tangut ideograph-" },
-  { start: 0x1b170, end: 0x1b2fb, prefix: "nushu character-" },
-  { start: 0xf900, end: 0xfa6d, prefix: "cjk compatibility ideograph-" },
-  { start: 0xfa70, end: 0xfad9, prefix: "cjk compatibility ideograph-" },
-  { start: 0x2f800, end: 0x2fa1d, prefix: "cjk compatibility ideograph-" },
+  { start: 0xac00, end: 0xd7a3, prefix: "HANGUL SYLLABLE " },
+  { start: 0x3400, end: 0x4db5, prefix: "CJK UNIFIED IDEOGRAPH-" },
+  { start: 0x4e00, end: 0x9fea, prefix: "CJK UNIFIED IDEOGRAPH-" },
+  { start: 0x20000, end: 0x2a6d6, prefix: "CJK UNIFIED IDEOGRAPH-" },
+  { start: 0x2a700, end: 0x2b734, prefix: "CJK UNIFIED IDEOGRAPH-" },
+  { start: 0x2b740, end: 0x2b81d, prefix: "CJK UNIFIED IDEOGRAPH-" },
+  { start: 0x2b820, end: 0x2cea1, prefix: "CJK UNIFIED IDEOGRAPH-" },
+  { start: 0x2ceb0, end: 0x2ebe0, prefix: "CJK UNIFIED IDEOGRAPH-" },
+  { start: 0x17000, end: 0x187ec, prefix: "TANGUT IDEOGRAPH-" },
+  { start: 0x1b170, end: 0x1b2fb, prefix: "NUSHU CHARACTER-" },
+  { start: 0xf900, end: 0xfa6d, prefix: "CJK COMPATIBILITY IDEOGRAPH-" },
+  { start: 0xfa70, end: 0xfad9, prefix: "CJK COMPATIBILITY IDEOGRAPH-" },
+  { start: 0x2f800, end: 0x2fa1d, prefix: "CJK COMPATIBILITY IDEOGRAPH-" },
 ]
 
 /**
