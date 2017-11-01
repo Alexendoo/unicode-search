7f9d88b7714b1801d2d8544ef2f8cef036972614
Alex Macleod <alex@macleod.io>
2017-11-01 11:59:49 +0000

    rename index.html -> index.ejs

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: "src/index.ejs",
     }),
     new webpack.HashedModuleIdsPlugin(),
   ],
