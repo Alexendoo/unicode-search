41d98e022878d609a7b7871dc738c3536da47908
Alex Macleod <alex@macleod.io>
2017-11-14 17:37:44 +0000

    throttle worker with Atomics.wait

--- src/ts/ui/index.ts
+++ src/ts/ui/index.ts
@@ -1,15 +1,16 @@
 import "../../files"
+import { InputMessage } from "../worker/messages"
 
 const worker = new Worker("worker.js")
 console.log(worker)
 
-const returns: any[] = []
 worker.onmessage = ev => {
-  returns.push(ev.data)
+  console.log(ev.data)
 }
 
-const sab = new SharedArrayBuffer(Uint32Array.BYTES_PER_ELEMENT)
-const uint32 = new Uint32Array(sab)
+const sab = new SharedArrayBuffer(2 * Uint32Array.BYTES_PER_ELEMENT)
+const uint32 = new Uint32Array(sab, 0, 1)
+const int32 = new Int32Array(sab, 4, 1)
 
 worker.postMessage(sab)
 
@@ -17,3 +18,21 @@ worker.postMessage(sab)
 window.worker = worker
 //@ts-ignore
 window.uint32 = uint32
+//@ts-ignore
+window.int32 = int32
+
+const input = document.getElementById("chars") as HTMLInputElement
+
+input.addEventListener("input", ev => {
+  const oldTag = Atomics.add(uint32, 0, 1)
+
+  Atomics.store(int32, 0, 2)
+  Atomics.wake(int32, 0, 1)
+
+  const message: InputMessage = {
+    input: input.value.toUpperCase(),
+    tag: oldTag + 1,
+  }
+
+  worker.postMessage(message)
+})
--- src/ts/worker/index.ts
+++ src/ts/worker/index.ts
@@ -1,28 +1,36 @@
 import { InputMessage } from "./messages"
 import { names } from "../data/names"
 
-let uint32: Uint32Array
+let tagMemory: Uint32Array
+let countMemory: Int32Array
 
 onmessage = event => {
   const message: InputMessage | SharedArrayBuffer = event.data
 
   if (message instanceof SharedArrayBuffer) {
-    uint32 = new Uint32Array(message)
+    tagMemory = new Uint32Array(message, 0, 1)
+    countMemory = new Int32Array(message, 4, 1)
     return
   }
 
   const start = performance.now()
 
+  let sent = 0
+
   for (const codepoint in names) {
-    if (Atomics.load(uint32, 0) !== message.tag) {
-      console.log("break")
+    if (Atomics.load(tagMemory, 0) !== message.tag) {
       break
     }
 
     const char = names[codepoint]
 
     if (char.includes(message.input)) {
-      postMessage(codepoint)
+      postMessage({
+        codepoint: parseInt(codepoint, 10),
+        tag: message.tag,
+      })
+      sent += 1
+      Atomics.wait(countMemory, 0, sent)
     }
   }
 
