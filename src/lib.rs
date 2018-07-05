f90114ca22cda1ab4dc92cd186ed938d98cb0e10
Alex Macleod <alex@macleod.io>
2018-07-05 14:03:19 +0100

    use no_std

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,70 @@
-#![feature(const_vec_new)]
+#![no_std]
+#![feature(alloc, core_intrinsics, panic_implementation, lang_items)]
+#![feature(const_fn, const_string_new, const_vec_new)]
+
+#[macro_use]
+extern crate alloc;
+extern crate wee_alloc;
+
+use alloc::Vec;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+extern "C" {
+    fn console_panic(ptr: *const ResultLocation<u8>);
+    fn console_oom();
+}
+
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(info: &core::panic::PanicInfo) -> ! {
+    unsafe {
+        static mut ERROR_LOCATION: ResultLocation<u8> = ResultLocation::new();
+
+        let stacktrace = format!("{}", info);
+
+        ERROR_LOCATION.ptr = stacktrace.as_ptr();
+        ERROR_LOCATION.len = stacktrace.len();
+
+        console_panic(&ERROR_LOCATION);
+
+        core::intrinsics::abort();
+    }
+}
+
+#[lang = "oom"]
+#[no_mangle]
+pub extern "C" fn oom() -> ! {
+    unsafe {
+        console_oom();
+
+        core::intrinsics::abort();
+    }
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() {}
 
 #[repr(C)]
 #[derive(Debug)]
-pub struct ResultLocation {
-    ptr: *const u32,
+pub struct ResultLocation<T> {
+    ptr: *const T,
     len: usize,
 }
 
+impl<T> ResultLocation<T> {
+    const fn new() -> Self {
+        ResultLocation {
+            ptr: core::ptr::null(),
+            len: 0,
+        }
+    }
+}
+
 static mut RESULT: Vec<u32> = Vec::new();
-static mut RESULT_LOCATION: ResultLocation = ResultLocation {
-    ptr: std::ptr::null(),
-    len: 0,
-};
+static mut RESULT_LOCATION: ResultLocation<u32> = ResultLocation::new();
 
 unsafe fn set_result_location() {
     RESULT_LOCATION.ptr = RESULT.as_ptr();
@@ -19,6 +72,6 @@ unsafe fn set_result_location() {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn location() -> *const ResultLocation {
+pub unsafe extern "C" fn location() -> *const ResultLocation<u32> {
     &RESULT_LOCATION
 }
