6142c92e094b1236128ccffad32007ea5381651d
Alex Macleod <alex@macleod.io>
2019-03-02 17:22:23 +0000

    Move lib into one file

--- src/bin/gen.rs
+++ src/bin/gen.rs
@@ -1,118 +1,120 @@
-/// Generates a variation of the suffix array for fast codepoint name searching
-/// by substring.
-///
-/// https://en.wikipedia.org/wiki/Suffix_array (note: Wikipedia uses 1-based
-/// indexing but here we will use 0-based for implementation parity)
-///
-/// Suffix arrays provide fast substring lookup into a string by allowing one
-/// to binary search for any suffix in said string, however for our purposes
-/// we need to search multiple strings (each Unicode character name), matching
-/// all codepoints whose names contain said substring. To achieve this we
-/// concatenate all the names together, storing the codepoint alongside the
-/// indexes.
-///
-/// For example the imaginary codepoints "one" as \x01 and "five" as \x05 would
-/// be joined to form S = "one$five$". '$' is a sentinel character that is
-/// lexicographically smaller than all other characters, in the implementation
-/// a newline character is used to make the generated file look nicer.
-///
-/// Because no pattern contains the sentinel, the suffix "ne$five$" is
-/// equivalent to "ne$", we also omit suffixes that begin with a sentinel, e.g.
-/// "$five$" (i = 3) or "$" (i = 8) to save some bytes.
-///
-/// Starting with the suffixes of S
-///
-/// | Suffix | i | c |
-/// | ------ | - | - |
-/// | one$   | 0 | 1 |
-/// | ne$    | 1 | 1 |
-/// | e$     | 2 | 1 |
-/// | five$  | 4 | 5 |
-/// | ive$   | 5 | 5 |
-/// | ve$    | 6 | 5 |
-/// | e$     | 7 | 5 |
-///
-/// They are then sorted lexicographically by suffix to become
-///
-/// | Suffix | i | c |
-/// | ------ | - | - |
-/// | e$     | 2 | 1 |
-/// | e$     | 7 | 5 |
-/// | five$  | 4 | 5 |
-/// | ive$   | 5 | 5 |
-/// | ne$    | 1 | 1 |
-/// | one$   | 0 | 1 |
-/// | ve$    | 6 | 5 |
-///
-/// Since we don't need to know where in the combined string our suffix is, only
-/// which codepoint it corresponds to we can join the entries for "e$", losing
-/// the information that there is a suffix at index 7. This costs a few bytes
-/// per entry since the codepoint array is now an array of arrays, however since
-/// there are around 18x fewer entries in total for this dataset the trade-off
-/// is well worth it.
-///
-/// After removing duplicates and the temporary suffixes the remaining data is:
-///
-/// - The combined string S = "one$five$",
-/// - The suffix array A,
-/// - The corresponding codepoints C
-///
-/// | i | A[i] | C[i] |
-/// | - | ---- | ---- |
-/// | 0 |    2 | 1, 5 |
-/// | 1 |    4 |    5 |
-/// | 2 |    5 |    5 |
-/// | 3 |    1 |    1 |
-/// | 4 |    0 |    1 |
-/// | 5 |    6 |    5 |
-///
-/// This is the final table that we will use to search for name matches. The
-/// suffix for each entry at i can be enumerated by iterating through the string
-/// S beginning at index A[i].
-///
-/// | i | S[A[i]..] | A[i] | C[i] |
-/// | - | -------   | ---- | ---- |
-/// | 0 | e$five$   |    2 | 1, 5 |
-/// | 1 | five$     |    4 |    5 |
-/// | 2 | ive$      |    5 |    5 |
-/// | 3 | ne$five$  |    1 |    1 |
-/// | 4 | one$five$ |    0 |    1 |
-/// | 5 | ve$       |    6 |    5 |
-///
-/// To find all the matches we have to find all the suffixes that begin with the
-/// input pattern. Since the table is sorted these suffixes are all contiguous,
-/// to locate them we perform two binary searches. One search to find the first
-/// matching suffix and for the last.
-///
-/// https://en.wikipedia.org/wiki/Suffix_array#Applications
-///
-/// With the range of suffixes found the list of codepoints can be enumerated
-/// for the interval C[first..last].
-///
-/// # Extra
-///
-/// Not shown is a further optimisation where each Unicode character name is
-/// split into individual words, e.g. "DIGIT ONE" adds "DIGIT$" and "ONE$" to
-/// the table separately. This allows us to deduplicate the many occurrences of
-/// words such as "LETTER" that appear in the middle of names. This reduction
-/// makes both the tables A and C smaller as well as the combined string S,
-/// drastically reducing the space requirement.
-///
-/// This means that multiword searches will no longer work directly since no
-/// suffixes contain spaces. Instead we also split the input pattern into words
-/// and search individually once for each word. Each word gives us a set of
-/// codepoints, the final result in the union of all word subresults.
-///
-/// For example "LATIN LETTER" is split into patterns "LATIN" and "LETTER",
-/// the final result is codepoints that match both "LATIN" and "LETTER". This
-/// means that word order of the input pattern no longer has an effect on the
-/// result, allowing "LATIN LETTER" to match "LATIN SMALL LETTER A".
-///
-/// Additionally, a small extra saving can be had by removing duplicate
-/// codepoints for a single suffix.  For example in "LEFT SQUARE BRACKET" when
-/// split into words "LEFT$", "SQUARE$" and "BRACKET$" the suffix "T$" appears
-/// twice. The "T$" suffix is already deduplicated however the codepoint for
-/// LEFT SQUARE BRACKET will appear twice in C[i], this is simply removed.
+//! Generates a variation of the suffix array for fast codepoint name searching
+//! by substring.
+//!
+//! https://en.wikipedia.org/wiki/Suffix_array (note: Wikipedia uses 1-based
+//! indexing but here we will use 0-based for implementation parity)
+//!
+//! Suffix arrays provide fast substring lookup into a string by allowing one
+//! to binary search for any suffix in said string, however for our purposes
+//! we need to search multiple strings (each Unicode character name), matching
+//! all codepoints whose names contain said substring. To achieve this we
+//! concatenate all the names together, storing the codepoint alongside the
+//! indexes.
+//!
+//! For example the imaginary codepoints "one" as \x01 and "five" as \x05 would
+//! be joined to form S = "one$five$". '$' is a sentinel character that is
+//! lexicographically smaller than all other characters, in the implementation
+//! a newline character is used to make the generated file look nicer.
+//!
+//! Because no pattern contains the sentinel, the suffix "ne$five$" is
+//! equivalent to "ne$", we also omit suffixes that begin with a sentinel, e.g.
+//! "$five$" (i = 3) or "$" (i = 8) to save some bytes.
+//!
+//! Starting with the suffixes of S
+//!
+//! | Suffix | i | c |
+//! | ------ | - | - |
+//! | one$   | 0 | 1 |
+//! | ne$    | 1 | 1 |
+//! | e$     | 2 | 1 |
+//! | five$  | 4 | 5 |
+//! | ive$   | 5 | 5 |
+//! | ve$    | 6 | 5 |
+//! | e$     | 7 | 5 |
+//!
+//! They are then sorted lexicographically by suffix to become
+//!
+//! | Suffix | i | c |
+//! | ------ | - | - |
+//! | e$     | 2 | 1 |
+//! | e$     | 7 | 5 |
+//! | five$  | 4 | 5 |
+//! | ive$   | 5 | 5 |
+//! | ne$    | 1 | 1 |
+//! | one$   | 0 | 1 |
+//! | ve$    | 6 | 5 |
+//!
+//! Since we don't need to know where in the combined string our suffix is, only
+//! which codepoint it corresponds to we can join the entries for "e$", losing
+//! the information that there is a suffix at index 7. This costs a few bytes
+//! per entry since the codepoint array is now an array of arrays, however since
+//! there are around 18x fewer entries in total for this dataset the trade-off
+//! is well worth it.
+//!
+//! After removing duplicates and the temporary suffixes the remaining data is:
+//!
+//! - The combined string S = "one$five$",
+//! - The suffix array A,
+//! - The corresponding codepoints C
+//!
+//! | i | A[i] | C[i] |
+//! | - | ---- | ---- |
+//! | 0 |    2 | 1, 5 |
+//! | 1 |    4 |    5 |
+//! | 2 |    5 |    5 |
+//! | 3 |    1 |    1 |
+//! | 4 |    0 |    1 |
+//! | 5 |    6 |    5 |
+//!
+//! This is the final table that we will use to search for name matches. The
+//! suffix for each entry at i can be enumerated by iterating through the string
+//! S beginning at index A[i].
+//!
+//! | i | S[A[i]..] | A[i] | C[i] |
+//! | - | -------   | ---- | ---- |
+//! | 0 | e$five$   |    2 | 1, 5 |
+//! | 1 | five$     |    4 |    5 |
+//! | 2 | ive$      |    5 |    5 |
+//! | 3 | ne$five$  |    1 |    1 |
+//! | 4 | one$five$ |    0 |    1 |
+//! | 5 | ve$       |    6 |    5 |
+//!
+//! To find all the matches we have to find all the suffixes that begin with the
+//! input pattern. Since the table is sorted these suffixes are all contiguous,
+//! to locate them we perform two binary searches. One search to find the first
+//! matching suffix and for the last.
+//!
+//! https://en.wikipedia.org/wiki/Suffix_array#Applications
+//!
+//! With the range of suffixes found the list of codepoints can be enumerated
+//! for the interval C[first..last].
+//!
+//! # Extra
+//!
+//! Not shown is a further optimisation where each Unicode character name is
+//! split into individual words, e.g. "DIGIT ONE" adds "DIGIT$" and "ONE$" to
+//! the table separately. This allows us to deduplicate the many occurrences of
+//! words such as "LETTER" that appear in the middle of names. This reduction
+//! makes both the tables A and C smaller as well as the combined string S,
+//! drastically reducing the space requirement.
+//!
+//! This means that multiword searches will no longer work directly since no
+//! suffixes contain spaces. Instead we also split the input pattern into words
+//! and search individually once for each word. Each word gives us a set of
+//! codepoints, the final result in the union of all word subresults.
+//!
+//! For example "LATIN LETTER" is split into patterns "LATIN" and "LETTER",
+//! the final result is codepoints that match both "LATIN" and "LETTER". This
+//! means that word order of the input pattern no longer has an effect on the
+//! result, allowing "LATIN LETTER" to match "LATIN SMALL LETTER A".
+//!
+//! Additionally, a small extra saving can be had by removing duplicate
+//! codepoints for a single suffix.  For example in "LEFT SQUARE BRACKET" when
+//! split into words "LEFT$", "SQUARE$" and "BRACKET$" the suffix "T$" appears
+//! twice. The "T$" suffix is already deduplicated however the codepoint for
+//! LEFT SQUARE BRACKET will appear twice in C[i], this is simply removed.
+
+use byteorder::{LittleEndian, WriteBytesExt};
 use std::collections::HashMap;
 use std::fs::create_dir_all;
 use std::fs::File;
@@ -126,15 +128,14 @@ use ucd_raw::CHARACTERS;
 #[derive(Debug)]
 struct Suffix<'a> {
     suffix: &'a str,
-    index: usize,
+    index: u32,
 }
 
 fn out_file<P: AsRef<Path>>(path: P) -> BufWriter<File> {
-    let path = path.as_ref();
+    let dir = Path::new("client/data");
+    let path = dir.join(path);
 
-    if let Some(parent) = path.parent() {
-        create_dir_all(parent).unwrap();
-    }
+    create_dir_all(dir).unwrap();
 
     let file = File::create(path).unwrap();
 
@@ -161,7 +162,7 @@ fn char_iter() -> impl Iterator<Item = &'static Character> {
         })
 }
 
-fn suffixes<'a>(string: &'a str) -> impl Iterator<Item = (usize, &'a str)> {
+fn suffixes(string: &str) -> impl Iterator<Item = (usize, &str)> {
     (0..string.len()).map(move |i| &string[i..]).enumerate()
 }
 
@@ -173,12 +174,15 @@ fn main() {
     let mut combined = String::new();
     let mut temp_suffixes = Vec::new();
 
-    let mut combined_out = out_file("client/data/combined3.txt");
-    let mut leb_out = out_file("client/data/table3.bin");
+    let mut combined_out = out_file("names.txt");
+    let mut table_out = out_file("table.bin");
+    let mut bounds = out_file("bounds.bin");
 
     for character in char_iter() {
-        let start = combined.len();
+        let start = combined.len() as u32;
         let name = character.name;
+
+        bounds.write_u32::<LittleEndian>(start).unwrap();
 
         combined.push_str(name);
         combined.push('\n');
@@ -186,7 +190,7 @@ fn main() {
         for (offset, suffix) in suffixes(name) {
             temp_suffixes.push(Suffix {
                 suffix,
-                index: start + offset,
+                index: start + offset as u32,
             });
         }
     }
@@ -196,11 +200,8 @@ fn main() {
     println!("length: {}", temp_suffixes.len());
 
     for suffix in temp_suffixes {
-        // leb128::write::unsigned(&mut leb_out, suffix.index as u64).unwrap();
-        let written = leb_out.write(&(suffix.index as u32).to_le_bytes()).unwrap();
-        assert_eq!(written, 4);
+        table_out.write_u32::<LittleEndian>(suffix.index).unwrap();
     }
-
     combined_out.write_all(combined.as_bytes()).unwrap();
 
     println!("Generated in {:?}", start.elapsed());
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-#[cfg(test)]
-mod test_data;
+#![warn(clippy::pedantic)]
 
-mod decoder;
-mod table;
-
+use std::mem;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -15,4 +12,94 @@ extern "C" {
 #[wasm_bindgen]
 pub fn init() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+}
+
+#[cfg(target_endian = "little")]
+unsafe fn cast_bytes(mut le_bytes: Vec<u8>) -> Vec<u32> {
+    let p = le_bytes.as_mut_ptr();
+    let len = le_bytes.len();
+    let cap = le_bytes.capacity();
+
+    assert_eq!(len % 4, 0);
+    assert_eq!(cap % 4, 0);
+
+    mem::forget(le_bytes);
+
+    #[allow(clippy::cast_ptr_alignment)]
+    Vec::from_raw_parts(p as *mut u32, len / 4, cap / 4)
+}
+
+#[wasm_bindgen]
+#[derive(Debug, Default)]
+pub struct Table {
+    text: Vec<u8>,
+    indicies: Vec<u32>,
+    bounds: Vec<u32>,
+}
+
+#[wasm_bindgen]
+impl Table {
+    #[wasm_bindgen(constructor)]
+    pub fn new(text: String, indicies: Vec<u8>, bounds: Vec<u8>) -> Self {
+        Self {
+            text: text.into_bytes(),
+            indicies: unsafe { cast_bytes(indicies) },
+            bounds: unsafe { cast_bytes(bounds) },
+        }
+    }
+
+    pub fn search(&self, substring: &str) -> Vec<u32> {
+        let indicies = self.find(substring.as_bytes());
+
+        indicies
+            .iter()
+            .map(|&i| self.index_to_codepoint(i))
+            .collect()
+    }
+
+    fn find(&self, substring: &[u8]) -> &[u32] {
+        // https://en.wikipedia.org/wiki/Suffix_array#Applications
+        let mut left = 0;
+        let mut right = self.indicies.len();
+
+        while left < right {
+            let mid = (left + right) / 2;
+            let index = self.indicies[mid] as usize;
+
+            if substring > self.slice_from(index, substring.len()) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        let start = left;
+        right = self.indicies.len();
+
+        while left < right {
+            let mid = (left + right) / 2;
+            let index = self.indicies[mid] as usize;
+
+            if substring < self.slice_from(index, substring.len()) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        &self.indicies[start..right]
+    }
+
+    fn slice_from(&self, start: usize, limit: usize) -> &[u8] {
+        let end = usize::min(start + limit, self.text.len());
+
+        &self.text[start..end]
+    }
+
+    fn index_to_codepoint(&self, index: u32) -> u32 {
+        match self.bounds.binary_search(&index) {
+            Ok(i) => self.bounds[i],
+            Err(i) => self.bounds[i - 1],
+        }
+    }
 }
