aaf78feb5fceb33af5bb56424b77bec8ab5e4131
Alex Macleod <alex@macleod.io>
2019-07-30 21:17:50 +0100

    Display results in fixed width columns

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import { FixedSizeList as List } from "react-window";
 
@@ -18,7 +18,23 @@ function useWindowHeight() {
     return height;
 }
 
-function Main({ searcher, names }) {
+function Entry({ style, index, parts }) {
+    const codepoint = parts.codepoints[index];
+
+    const char = String.fromCodePoint(codepoint);
+    const name = parts.names[index];
+    const codepointString = "U+" + codepoint.toString(16).toUpperCase();
+
+    return (
+        <div className="char" style={style}>
+            <span className="codepoint">{codepointString}</span>
+            <span className="literal">{char}</span>
+            <span className="name">{name}</span>
+        </div>
+    );
+}
+
+function Main({ searcher, parts }) {
     const [pattern, setPattern] = useState("");
     const height = useWindowHeight() - 50;
 
@@ -27,62 +43,83 @@ function Main({ searcher, names }) {
         resultIndicies = searcher.indicies(pattern.toUpperCase());
     }
 
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        function focusInput() {
+            inputRef.current.focus();
+        }
+
+        document.addEventListener("keydown", focusInput);
+
+        return () => document.removeEventListener("keydown", focusInput);
+    }, []);
+
     return (
         <div>
             <div className="input-bar">
                 <span>{">"}</span>
                 <input
-                    autoFocus
                     autoComplete="off"
+                    autoFocus
+                    onChange={e => setPattern(e.target.value)}
+                    ref={inputRef}
                     type="text"
                     value={pattern}
-                    onChange={e => setPattern(e.target.value)}
                 />
             </div>
             <List
                 height={height}
                 width="100%"
                 itemCount={resultIndicies.length}
-                itemSize={32}
+                itemSize={24}
             >
                 {({ index, style }) => (
-                    <div style={style}>{names[resultIndicies[index]]}</div>
+                    <Entry
+                        style={style}
+                        index={resultIndicies[index]}
+                        parts={parts}
+                    />
                 )}
             </List>
         </div>
     );
 }
 
-async function download(name, format = "bytes") {
+async function download(name, BufferType) {
     const response = await fetch(`/data/${name}`);
 
-    if (format === "text") {
+    if (!BufferType) {
         return response.text();
     } else {
         const buffer = await response.arrayBuffer();
 
-        return new Uint8Array(buffer);
+        return new BufferType(buffer);
     }
 }
 
 (async function() {
     const [pkg, indicies, codepoints, namesCombied, table] = await Promise.all([
         import("../pkg/utf.js"),
-        download("indicies.bin"),
-        download("codepoints.bin"),
-        download("names.txt", "text"),
-        download("table.bin"),
+        download("indicies.bin", Uint8Array),
+        download("codepoints.bin", Uint32Array),
+        download("names.txt"),
+        download("table.bin", Uint8Array),
     ]);
 
     pkg.set_panic_hook();
 
     const searcher = new pkg.Searcher(namesCombied, table, indicies);
-    window.s = searcher;
 
     const names = namesCombied.split("\n");
 
+    const parts = { names, codepoints };
+
+    window.s = searcher;
+    window.parts = parts;
+
     render(
-        <Main searcher={searcher} names={names} />,
+        <Main searcher={searcher} parts={parts} />,
         document.querySelector("main"),
     );
 })();
