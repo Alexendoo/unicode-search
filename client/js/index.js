898507f0f254b10a4606c1815f3aee5f51160b01
Alex Macleod <alex@macleod.io>
2019-09-08 15:54:15 +0100

    Replace react-window with own root scroller

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 import { render } from "react-dom";
-import { FixedSizeList as List } from "react-window";
 
 function useWindowHeight() {
     const [height, setHeight] = useState(() => window.innerHeight);
@@ -34,6 +33,64 @@ function Entry({ style, index, parts }) {
     );
 }
 
+function List({ indicies, parts }) {
+    const itemHeight = 24;
+
+    const root = useRef(null);
+    const [range, setRange] = useState({
+        start: 0,
+        length: 0,
+    });
+
+    useLayoutEffect(() => {
+        function calculateRange() {
+            const bounds = root.current.getBoundingClientRect();
+
+            const scrolledBy = Math.max(0, -bounds.top);
+
+            const start = Math.floor(scrolledBy / itemHeight);
+            const length = Math.ceil(window.innerHeight / itemHeight);
+
+            setRange({ length, start });
+        }
+
+        calculateRange();
+
+        window.addEventListener("scroll", calculateRange);
+
+        return () => window.removeEventListener("scroll", calculateRange);
+    }, [indicies]);
+
+    const style = {
+        height: itemHeight * indicies.length,
+        position: "relative",
+    };
+
+    const items = Array.from(
+        {
+            length: Math.min(range.length, indicies.length),
+        },
+        (_, i) => {
+            const index = indicies[i + range.start];
+
+            const style = {
+                position: "absolute",
+                top: (i + range.start) * itemHeight,
+            };
+
+            return (
+                <Entry index={index} parts={parts} key={index} style={style} />
+            );
+        },
+    );
+
+    return (
+        <div style={style} ref={root}>
+            {items}
+        </div>
+    );
+}
+
 function Main({ searcher, parts }) {
     const [pattern, setPattern] = useState("");
     const height = useWindowHeight() - 50;
@@ -55,20 +112,7 @@ function Main({ searcher, parts }) {
                     value={pattern}
                 />
             </div>
-            <List
-                height={height}
-                width="100%"
-                itemCount={resultIndicies.length}
-                itemSize={24}
-            >
-                {({ index, style }) => (
-                    <Entry
-                        style={style}
-                        index={resultIndicies[index]}
-                        parts={parts}
-                    />
-                )}
-            </List>
+            <List indicies={resultIndicies} parts={parts} />
         </div>
     );
 }
