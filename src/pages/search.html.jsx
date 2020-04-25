2080aa6ead291681769c152bfd0ee51180dd4da8
Alex Macleod <alex@macleod.io>
2020-04-25 17:34:26 +0100

    Preload wasm, names, codepoints

--- src/pages/search.html.jsx
+++ src/pages/search.html.jsx
@@ -5,13 +5,23 @@ import { assetLoader, renderStatic, Head } from "./shared";
 export default ({ compilation }) => {
     const asset = assetLoader(compilation);
 
+    const FetchPreload = ({ re }) => (
+        <link
+            rel="preload"
+            href={asset(re)}
+            as="fetch"
+            crossOrigin="anonymous"
+        />
+    );
+
     const app = (
         <html lang="en">
             <Head>
                 <title>Character Information Search</title>
 
-                {/* TODO: crossorigin needs to match */}
-                <link rel="preload" href={asset(/.wasm$/)} as="fetch" />
+                <FetchPreload re={/wasm$/} />
+                <FetchPreload re={/codepoints.*bin$/} />
+                <FetchPreload re={/names.*txt$/} />
 
                 <link rel="stylesheet" href={asset(/css$/)} />
                 <script defer src={asset(/search.*js$/)} />
