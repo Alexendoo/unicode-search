94b92e1c033e010415cc22f194b77cffb23bf9c2
Alex Macleod <alex@macleod.io>
2020-08-30 19:55:16 +0100

    Target modern browsers

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    presets: ["@babel/preset-env"],
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    android: "84",
+                    chrome: "70",
+                    edge: "84",
+                    firefox: "80",
+                    ios: "12",
+                    safari: "12",
+                },
+            },
+        ],
+    ],
 };
